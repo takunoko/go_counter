87b04a1f7447b562af07d6d838fb2e03378e15b9
takunoko <takunoko@gmail.com>
2023-04-23 17:33:30 +0900

    redisの出力をhttpで返すように修正

--- infrastructure/redis/redis.go
+++ infrastructure/redis/redis.go
@@ -5,40 +5,45 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/redis/go-redis/v9"
+	"net/http"
 	"strconv"
 )
 
 var ctx = context.Background()
 
-func RedisClientTest(echo.Context) error {
+func RedisClientTest(echoCtx echo.Context) error {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	err := rdb.Set(ctx, "key", "123", 0).Err()
+	err := rdb.Set(ctx, "k", "123", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := rdb.Get(ctx, "key").Result()
+	retStr := "set key: k, val: 123\n"
+
+	val, err := rdb.Get(ctx, "k").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("val: ", val)
+	retStr += fmt.Sprintf("get key: k, val: %s\n", val)
 
 	val = strconv.FormatInt(rdb.Incr(ctx, "key").Val(), 10)
-	fmt.Println("val: ", val)
+	retStr += fmt.Sprintf("incr key: k, val: %s\n", val)
 
 	val2, err := rdb.Get(ctx, "key2").Result()
 	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
+		retStr += "key: key2 is not found\n"
 	} else if err != nil {
 		panic(err)
 	} else {
-		fmt.Println("key2", val2)
+		retStr += fmt.Sprintf("incr key2: k, val: %s\n", val2)
 	}
+
+	echoCtx.String(http.StatusOK, retStr)
 
 	// Output: key value
 	// key2 does not exist
